f60fe59405426f439770fd6fae9f3d7e7ede3bb5
macman <hayrapetyan.hamlet@gmail.com>
2020-09-08 13:33:49 +0200

    finish iterations 5,6

--- starter-code/routes/celebrities.js
+++ starter-code/routes/celebrities.js
@@ -40,6 +40,47 @@ router.get('/celebrities/:id', (req, res) => {
     }). catch(error => {
       console.log(error);
     })
+  });
+
+  router.get('/celebrities/edit/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(id);
+    Celebrity.findById(id)
+      .then(celebrityFromDB => {
+        // render an edit form with the data from the book
+        console.log(celebrityFromDB);
+        res.render('celebrities/celebrityEditForm', { celebrity: celebrityFromDB })
+      })
+      .catch(error => {
+        console.log(error);
+      })
+  });
+
+  router.get('/celebrities/delete/:id', (req, res) => {
+    const id = req.params.id
+    Celebrity.findByIdAndDelete(id)
+    .then(() => {
+      res.redirect('/celebrities');
+    })
+    .catch(error => {
+      console.log(error);
+    })
+  })
+
+  router.post('/celebrities/edit/:id', (req, res) => {
+    const {name, occupation, catchPhrase} = req.body;
+    const id = req.params.id;
+    Celebrity.findByIdAndUpdate(id, {
+      name: name,
+      occupation: occupation,
+      catchPhrase: catchPhrase,
+    })
+    .then(celebrity => {
+      res.redirect(`/celebrities/${celebrity._id}`)
+    })
+    .catch(error => {
+      console.log(err);
+    })
   })
 
 module.exports = router;
